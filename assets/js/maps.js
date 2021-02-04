c711f0eb02e2a23b811305a6183f0942353c4161
RuiOliveira83 <rui.p3ld@gmail.com>
2021-02-04 15:16:10 +0000

    Added Google Maps with Markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -7,6 +7,7 @@ function initMap() {
         }
     });
 
+
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
@@ -15,12 +16,14 @@ function initMap() {
         { lat: 40.754932, lng: -73.984016 }
     ];
 
-    var markers = locations.map(function(location, i) {
+    var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
 }
